9c3e27baf7379f0b51cdb2cfb5f057cbd533aa07
Serzh108 <136076826+Serzh108Tetra@users.noreply.github.com>
2024-11-12 12:55:28 +0200

    EDIT | added more components

--- src/app/components/add-company-button.tsx
+++ src/app/components/add-company-button.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import dynamic from 'next/dynamic';
 import Button from '@/app/components/button';
+
 const CompanyFormModal = dynamic(() => import('./company-form-modal'), {
   ssr: false,
 });
