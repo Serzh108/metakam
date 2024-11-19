50664ecb9a7af6fd1fdea2c9aef6c97ff51a4e7a
Serzh108 <136076826+Serzh108Tetra@users.noreply.github.com>
2024-11-19 18:24:56 +0200

    FIX | fixed type error for params

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -3,16 +3,16 @@ import React from 'react';
 import Header from '@/app/components/header';
 // import { notFound } from 'next/navigation';
 
-type PageProps = {
-  params: { id: string };
-};
+// type PageProps = {
+//   params: { id: string };
+// };
 
 // export function generateStaticParams() {
 //   return [{ id: '1' }, { id: '2' }, { id: '3' }];
 // }
 
-export default async function Page({ params }: PageProps) {
-  // export default function Page({ params }: { params: any }) {
+// export default async function Page({ params }: PageProps) {
+export default function Page({ params }: { params: any }) {
   // useEffect(() => {
   //   const id = Number.parseInt(params.id);
   //   if (Number.isNaN(id)) {
@@ -20,8 +20,9 @@ export default async function Page({ params }: PageProps) {
   //   }
   // }, [params.id]);
 
-  const { id } = await params;
-  // const { id } = params;
+  // const { id } = await params;
+  const { id } = params;
+
   return (
     <>
       <Header>Company ({id})</Header>
