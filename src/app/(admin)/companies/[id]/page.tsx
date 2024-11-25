fa5610f5a9e9692dae38f3341393f6ffeb477188
Serzh108 <136076826+Serzh108Tetra@users.noreply.github.com>
2024-11-25 23:08:00 +0200

    EDIT | added new func and fixed errors

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -1,7 +1,10 @@
-// 'use client';
 import React from 'react';
-// import Header from '@/app/components/header';
-// import { notFound } from 'next/navigation';
+import { notFound } from 'next/navigation';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { Company, getCompany, getPromotions } from '@/lib/api';
+import getQueryClient from '@/lib/utils/getQueryClient';
+import CompanyInfo from '@/app/components/company-info';
+import CompanyPromotions from '@/app/components/company-promotions';
 
 type PageProps = {
   params: Promise<{ id: string }>;
@@ -12,18 +15,38 @@ type PageProps = {
 // }
 
 export default async function Page({ params }: PageProps) {
-  // useEffect(() => {
-  //   const id = Number.parseInt(params.id);
-  //   if (Number.isNaN(id)) {
-  //     notFound();
-  //   }
-  // }, [params.id]);
-
   const { id } = await params;
+  const queryClient = getQueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['companies', id],
+    queryFn: () => getCompany(id, { cache: 'no-store' }),
+    staleTime: 10 * 1000,
+  });
+
+  await queryClient.prefetchQuery({
+    queryKey: ['promotions', id],
+    queryFn: () => getPromotions({ companyId: id }, { cache: 'no-store' }),
+    staleTime: 10 * 1000,
+  });
+
+  const company = queryClient.getQueryData(['companies', id]) as Company;
+  if (!company) {
+    notFound();
+  }
+
+  const dehydratedState = dehydrate(queryClient);
 
   return (
-    <div className="py-6 px-10">
-      <p>{`Information about company (${id})`}</p>
-    </div>
+    <HydrationBoundary state={dehydratedState}>
+      <div className="py-6 px-10 grid grid-cols-12 gap-5">
+        <div className="col-span-3">
+          <CompanyInfo companyId={id} />
+        </div>
+        <div className="col-span-9">
+          <CompanyPromotions companyId={id} />
+        </div>
+      </div>
+    </HydrationBoundary>
   );
 }
